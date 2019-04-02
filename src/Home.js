6631eac9d61bfa34c1e4de0f1317f7e524b99e60
Kristel Fung <kristelfung@gmail.com>
2019-04-01 22:16:11 -0400

    font and sass working

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import FAQ from './FAQ';
+import "./scss/styles.scss";
 
 class Home extends Component {
   render() {
