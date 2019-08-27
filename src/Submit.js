a850ed661f0fe5e9007452968fcfac32c1ac7f0c
Kristel Fung <kristelfung@gmail.com>
2019-08-27 16:45:56 +0800

    finish form

--- src/Submit.js
+++ src/Submit.js
@@ -7,19 +7,64 @@ const Submit = () => {
         <h1>Submit</h1>
         <p>Send in a recipe which may or may not be published!</p>
       </div>
-      <form name="contact" method="post">
-        <input type="hidden" name="form-name" value="contact" />
+      <form name="submit" method="post">
+        <input type="hidden" name="form-name" value="submit" />
         <p>
-          <label>Your Name: <input type="text" name="name"/></label>
+          <label>
+            Recipe name
+            <input type="text" name="recipe-name" placeholder="Avocado pasta"/>
+          </label>
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email"/></label>
+          <label>
+            Author
+            <input type="text" name="author" placeholder="John"/>
+          </label>
         </p>
         <p>
-          <label>Message: <textarea name="message"></textarea></label>
+          <label>
+            Time
+            <input type="text" name="time" placeholder="20 min"></input>
+          </label>
         </p>
         <p>
-          <button type="submit">Send</button>
+          <label>
+            Cost
+            <input type="text" name="cost" placeholder="$3.50"></input>
+          </label>
+        </p>
+        <p>
+          <label>
+            Cookware needed
+            <input type="number" name="cookware" placeholder="4"></input>
+          </label>
+        </p>
+        <p>
+          <label>
+            Servings
+            <input type="number" name="servings" placeholder="4"></input>
+          </label>
+        </p>
+        <p>
+          <label>
+            Ingredients
+            <textarea rows="10" name="ingredients" placeholder="1 avocado, 1 box of pasta..."></textarea>
+          </label>
+        </p>
+        <p>
+          <label>
+            Cookware
+            <textarea rows="10" name="cookware" placeholder="1 frying pan, 1 pasta pot..."></textarea>
+          </label>
+        </p>
+        <p>
+          <label>
+            Directions
+            <textarea rows="10" name="directions" placeholder="1. Cut open avocado and mash contents in pan. 2. Add olive oil..."></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit" className="button button--primary">Send</button>
         </p>
       </form>
     </div>
