22b8f009633d5f1660f6477fb68779da5649eb2d
Kristel Fung <kristelfung@gmail.com>
2020-09-06 19:55:04 +0800

    Make RecipeCard stateless

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class RecipeCard extends Component {
-  render() {
+const RecipeCard = (props) => {
     return (
-      <div className="column">
-        <Link to={this.props.urlify(this.props.recipe.name)} className="link-wrapper">
-          <div className="recipe-card" 
-            style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + this.props.recipe.image.fields.file.url + ")"}}>
-            <span role="img" aria-label="clock" className="emoji emoji--clock">⏰</span>
-            <span className="time">{this.props.recipe.time} mins</span>
-            <span className="cost">${this.props.recipe.cost}</span>
-            <h2>{this.props.recipe.name}</h2>
-          </div>
-        </Link>
-      </div>
-    )
-  }
+    <div className="column">
+      <Link to={props.urlify(props.recipe.name)} className="link-wrapper">
+        <div className="recipe-card" 
+          style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + props.recipe.image.fields.file.url + ")"}}>
+          <span role="img" aria-label="clock" className="emoji emoji--clock">⏰</span>
+          <span className="time">{props.recipe.time} mins</span>
+          <span className="cost">${props.recipe.cost}</span>
+          <h2>{props.recipe.name}</h2>
+        </div>
+      </Link>
+    </div>
+  )
 }
 
 export default RecipeCard;
